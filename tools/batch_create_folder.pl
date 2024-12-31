592c69f8b66e91d70f4a773d9ab56206860b163d
Bill Ho <ycho.bill.lab@gmail.com>
2024-12-31 17:56:11 +0800

    chore: docs:  updated script batch_create_folder.pl

--- tools/batch_create_folder.pl
+++ tools/batch_create_folder.pl
@@ -73,7 +73,7 @@ batch_create_folder.pl - Create folder batchly for downloading related genomes l
   
   Use datasets download virus genome taxon <command> --help for detailed help about a command.
 
-=head3 Example Case 3 - create folder (if folder is not existed), and split genome via csv file genomes_of_nidovirales.csv and tool faSomeRecords (please prepare csv and download tool first)
+=head3 Example Case 3 - create folder (if folder is not existed), and split genome via csv file genomes_of_nidovirales.csv and tool faSomeRecords (please prepare csv, download tool from http://hgdownload.soe.ucsc.edu/admin/exe/linux.x86_64/faSomeRecords and add it into your PATH environment first)
 
   $ head families_of_nidovirales.txt genomes_of_nidovirales.csv
   ==> families_of_nidovirales.txt <==
@@ -165,3 +165,37 @@ while(<>) {
   }
 
 }
+
+__END__
+
+=head1 AUTHORS
+
+Bill Ho E<lt>ycho.bill.lab@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2024 by Bill Ho E<lt>ycho.bill.lab@gmail.comE<gt>.
+
+This software is released under the MIT license cited below.
+
+=head2 The "MIT" License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+
+=cut
